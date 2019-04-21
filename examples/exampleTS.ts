e918a2d9047a3006ab11e2aca9f5a6e9c9d3ae90
Benjamin Vincent <luxcium@neb401.com>
2019-04-21 10:35:24 -0400

    adding TS styles (colours)

--- examples/exampleTS.ts
+++ examples/exampleTS.ts
@@ -30,7 +30,7 @@ interface IInterface {
 }
 
 async function asyncFunction() {
-  const variable1 = 10;
+  const variable1 = 2 + 10;
   let variable2: string = "this is a string indeed";
   variable2 = variable1.toFixed(2);
   return variable2;
