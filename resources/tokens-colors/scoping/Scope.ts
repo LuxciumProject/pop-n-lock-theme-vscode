82efb42688a3c8d45bbdf37f5a0cd9855ec14929
Benjamin Vincent <luxcium@neb401.com>
2019-06-08 00:22:49 -0400

    changes mades will publish new colors ...

--- resources/tokens-colors/scoping/Scope.ts
+++ resources/tokens-colors/scoping/Scope.ts
@@ -1,21 +1,41 @@
-export class Scope implements Scopes {
-  public source: string;
-  private elementName: string[];
-  constructor(elementName: string[] | string) {
-    if (typeof elementName === 'string') {
-      this.elementName = [elementName];
-    } else {
-      this.elementName = elementName;
-    }
-  }
-  public toString() {
-    if (this.elementName.length === 1) {
-      return `${this.source} ${this.elementName[0]}`;
-    }
-    return `${this.source} ${this.elementName.join('.')}`;
-  }
-}
+//   public static stringify(value: Scope) {
+//     return value;
+//   }
+//   public static parse(value: Scope) {
+//     return value;
+//   }
+//   public source: string;
+//   private elementName: string[];
+//   constructor(elementName: string[] | string) {
+//     if (typeof elementName === 'string') {
+//       this.elementName = [elementName];
+//     } else {
+//       this.elementName = elementName;
+//     }
+//   }
+//   public toString() {
+//     if (this.elementName.length === 1) {
+//       return `${this.source} ${this.elementName[0]}`;
+//     }
+//     return `${this.source} ${this.elementName.join('.')}`;
+//   }
+
+
+
+// 'source.ts string.constant.other.placeholder',
+// 'source.ts string.constant.other.placeholder',
+
+export class Scope implements Scopes {}
+export class Element implements Elements  {}
+export class Selector implements Selectors {}
+export class Source extends Element {}
 
 export interface Scopes {
-  source: string;
-}
+    source?: string;
+  }
+  export interface Elements {
+    source?: string;
+  }
+  export interface Selectors {
+    source?: string;
+  }
