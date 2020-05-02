81e2fdc6e741df62546126c3ebe2b3b189e7763c
Benjamin Vincent <luxcium@neb401.com>
2020-05-02 13:27:14 -0400

    working on next version
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

---
+++ src/TypeScript/terminal-colors.ts
@@ -0,0 +1,18 @@
+export const terminalColors = {
+  'terminal.ansiBlack': '#221B25',
+  'terminal.ansiBlue': '#458588',
+  'terminal.ansiCyan': '#689E6A',
+  'terminal.ansiGreen': '#98971A',
+  'terminal.ansiMagenta': '#B16286',
+  'terminal.ansiRed': '#CC241D',
+  'terminal.ansiWhite': '#C0B0A0',
+  'terminal.ansiYellow': '#E7A931',
+  'terminal.ansiBrightBlack': '#928374',
+  'terminal.ansiBrightBlue': '#99C6CA',
+  'terminal.ansiBrightCyan': '#8AE180',
+  'terminal.ansiBrightGreen': '#B5C033',
+  'terminal.ansiBrightMagenta': '#D66282',
+  'terminal.ansiBrightRed': '#F42C3E',
+  'terminal.ansiBrightWhite': '#DDCCAA',
+  'terminal.ansiBrightYellow': '#FABE2F',
+};
